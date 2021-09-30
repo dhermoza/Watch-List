3ae7df30977814a7027d1d8272e5c54e4c1c52b2
Denise Hermoza <denise.hmm91@gmail.com>
2021-09-30 15:51:26 -0500

    funcionality working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'lists/', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new', as: :new_list
+  # get 'lists/:id', to: 'lists#show', as: :list
+  # post 'lists', to: 'lists#create'
+  # get 'lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: :new_bookmark
+  # post 'lists/:list_id/bookmarks', to: 'bookmark#index', as: :show_bookmark
+  # delete 'bookmarks/:list_id', to: 'bookmark#destroy'
+  resources :lists do
+    resources :bookmarks, only: [ :new, :create, :show]
+  end
+  resources :bookmarks, only: [:destroy]
 end
